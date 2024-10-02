ea6c9897f382f507168c868adf958d99d520c52e
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-02 22:47:56 +0300

    Update vite.config.ts to disable clear screen on build

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
