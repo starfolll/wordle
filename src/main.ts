957c25d63830cd5c14446933c2e72b05c57de681
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-02 22:47:10 +0300

    Add fontawesome icons and update base styles

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import './assets/main.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { createPinia } from 'pinia'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/base.css'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
