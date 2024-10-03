7feb1857f6644b534acb1a35e846f7146b36721a
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-03 16:42:46 +0300

    added bounce effect to wordCell

--- src/components/wordRows/GuessingRow.vue
+++ src/components/wordRows/GuessingRow.vue
@@ -8,11 +8,17 @@ const wordleStore = useWordleStore()
 
 const getClearWord = () => Array.from<null>({ length: wordleStore.lettersInWord }).fill(null)
 const inputs = ref<HTMLInputElement[]>([])
+const wordCells = ref<typeof WordCell[]>([])
 const guessingWord = ref<Array<string | null>>(getClearWord())
 
 const isAllowedLetter = (char: string) => /[a-z]/i.test(char)
 const focusInput = (index: number) => requestAnimationFrame(() => inputs.value[index]?.focus())
 
+function setGuessingWordLetter(letter: string | null, index: number) {
+  guessingWord.value[index] = letter ? letter.toLowerCase() : letter
+  wordCells.value[index].container.classList.remove('bounce')
+  requestAnimationFrame(() => wordCells.value[index].container.classList.add('bounce'))
+}
 function clearGuessingWord() {
   guessingWord.value = getClearWord()
   focusInput(0)
@@ -25,7 +31,7 @@ function onInput(e: Event, index: number) {
   if (!isAllowedLetter(value) && value !== ' ')
     return input.value = ''
 
-  guessingWord.value[index] = value === ' ' ? null : value.toLowerCase()
+  setGuessingWordLetter(value, index)
   focusInput(index + 1)
 }
 
@@ -36,13 +42,13 @@ function onKeyDown(e: KeyboardEvent, index: number) {
       focusInput(0)
     }
     else {
-      guessingWord.value[index] = null
+      setGuessingWordLetter(null, index)
       focusInput(index - 1)
     }
   }
 
   else if (e.key.length === 1 && isAllowedLetter(e.key) && guessingWord.value[index] !== null) {
-    guessingWord.value[index] = e.key
+    setGuessingWordLetter(e.key, index)
     focusInput(index + 1)
   }
 
@@ -69,7 +75,7 @@ onMounted(() => {
 
 <template>
   <WordsContainer v-if="wordleStore.word">
-    <WordCell v-for="(_, index) in guessingWord" :key="index">
+    <WordCell v-for="(_, index) in guessingWord" ref="wordCells" :key="index">
       <input
         ref="inputs"
         :value="guessingWord[index]"
@@ -82,3 +88,18 @@ onMounted(() => {
     </WordCell>
   </WordsContainer>
 </template>
+
+<style scoped>
+@keyframes bounce {
+  0% { scale: 1; }
+  50% { scale: 1.1; }
+  100% { scale: 1; }
+}
+
+.bounce {
+  animation-name: bounce;
+  animation-duration: 0.2s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+}
+</style>
--- src/components/wordRows/WordCell.vue
+++ src/components/wordRows/WordCell.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const container = ref<HTMLDivElement | null>(null)
+
+defineExpose({ container })
+</script>
+
 <template>
-  <div class="grid items-center justify-center text-3xl font-bold uppercase rounded-sm w-14 aspect-square bg-neutral-800">
+  <div ref="container" class="grid items-center justify-center text-3xl font-bold uppercase rounded-sm w-14 aspect-square bg-neutral-800">
     <slot />
   </div>
 </template>
