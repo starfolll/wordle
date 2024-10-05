4beedb597af4669d89ebc6a7efc52ff8ccd0b388
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-05 22:03:13 +0300

    Refactor vGlobalKeyPress directive to use a Map instead of a WeakMap for storing event listeners

--- src/directives/animations/v-global-key-press.ts
+++ src/directives/animations/v-global-key-press.ts
@@ -3,26 +3,28 @@ import type { Directive } from 'vue'
 type TListenerEvent = (el: HTMLElement, e: KeyboardEvent) => void
 type TKeydownListener = (e: KeyboardEvent) => void
 
-const weakMapElToKeydownListener = new WeakMap<any, TKeydownListener>()
+const listenerToKeydownListenerMap = new Map<TListenerEvent, TKeydownListener>()
 
 export const vGlobalKeyPress: & Directive<any, TListenerEvent> = {
   mounted(el, binding) {
-    const listener = weakMapElToKeydownListener.set(el, e => binding.value(el, e)).get(el)!
+    const listener = listenerToKeydownListenerMap.set(el, e => binding.value(el, e)).get(el)!
     window.addEventListener('keydown', listener)
   },
 
   updated(el, binding) {
-    const oldListener = weakMapElToKeydownListener.get(el)
+    const oldListener = listenerToKeydownListenerMap.get(el)
     if (oldListener)
       window.removeEventListener('keydown', oldListener)
 
-    const listener = weakMapElToKeydownListener.set(el, e => binding.value(el, e)).get(el)!
+    const listener = listenerToKeydownListenerMap.set(el, e => binding.value(el, e)).get(el)!
     window.addEventListener('keydown', listener)
   },
 
   beforeUnmount(el) {
-    const listener = weakMapElToKeydownListener.get(el)
-    if (listener)
+    const listener = listenerToKeydownListenerMap.get(el)
+    if (listener) {
       window.removeEventListener('keydown', listener)
+      listenerToKeydownListenerMap.delete(el)
+    }
   },
 }
