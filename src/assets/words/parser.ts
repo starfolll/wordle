b0b08ffea9aa912d5f85020e711bde323db2c54f
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-08 22:41:42 +0300

    added words parser

---
+++ src/assets/words/parser.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+import { parse } from 'node-html-parser'
+
+// TODO: use http://www.wordcyclopedia.com/english/a1
+
+const levels = ['a1', 'a2', 'b1', 'b2', 'c1']
+
+async function fetchWords(level: string) {
+  console.log(`Fetching words for level ${level}...`)
+  const vocabulary = await fetch(`http://www.wordcyclopedia.com/english/${level}`)
+  const html = await vocabulary.text()
+  const root = parse(html)
+
+  console.log(`Parsing words for level ${level}...`)
+  return Array.from(root.querySelectorAll('.word')).map(word => word.textContent as string)
+}
+
+async function saveWords(level: string, words: string[]) {
+  const jsonFileContext = JSON.stringify(words, null, 2)
+
+  console.log(`Saving words for level ${level}...`)
+  await Bun.write(`${import.meta.dirname}/${level}.json`, jsonFileContext)
+}
+
+Promise.all(levels.map(level => fetchWords(level).then(words => saveWords(level, words))))
