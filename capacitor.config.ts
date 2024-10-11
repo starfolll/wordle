c29bfc0ceb8c3781bb21181cd56c280d811b5212
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-11 18:10:26 +0300

    Added android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'wordle',
+  webDir: './services/app/dist',
+}
+
+export default config
