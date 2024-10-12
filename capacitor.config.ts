b4138e6cdeac96e05e75bef64f1259b51e49d3f1
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-12 15:09:35 +0300

    Removed android

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'wordle',
-  webDir: './services/app/dist',
-}
-
-export default config
