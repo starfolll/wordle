ba18e505152a93cc166d851fc9dd8fbb74a8a3ee
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-22 16:31:23 +0300

    Moved prisma to libs

---
+++ libs/prisma-client/index.ts
@@ -0,0 +1,24 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+
+export const prismaClient = new PrismaClient()
+
+export async function prismaHealthCheck() {
+  try {
+    await prismaClient.$queryRaw`SELECT 1`
+    return { connected: true }
+  }
+  catch (e) {
+    return { error: e }
+  }
+}
+
+export async function nullIfKnownError<T>(promise: Promise<T>): Promise<T | null> {
+  try {
+    return await promise
+  }
+  catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2025')
+      return null
+    throw e
+  }
+}
