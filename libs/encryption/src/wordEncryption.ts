517fa54bf8bed135de7c7e01e49fd69ab219cca0
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-10-26 02:03:51 +0300

    Fixed decryptWord returning wrong result

--- libs/encryption/src/wordEncryption.ts
+++ libs/encryption/src/wordEncryption.ts
@@ -1,9 +1,10 @@
+import cryptoUtf8 from 'crypto-js/enc-utf8'
 import rabbit from 'crypto-js/rabbit'
 
 export function encryptWord(word: string, secret: string) {
-  return rabbit.encrypt(word, secret)
+  return rabbit.encrypt(word, secret).toString()
 }
 
 export function decryptWord(word: string, secret: string) {
-  return rabbit.decrypt(word, secret)
+  return rabbit.decrypt(word, secret).toString(cryptoUtf8)
 }
