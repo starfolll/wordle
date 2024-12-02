cc1bc19d0736f2aeed17e1feabd8e1c0912abfce
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-12-02 18:59:25 +0200

    remove unused components: delete ContainsTooltip, DailyChallenge, and Dialog for codebase cleanup

--- services/app/src/components/ContainsTooltip.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="relative">
-    <slot name="default" />
-
-    <div class="absolute text-sm text-nowrap -translate-x-1/2 pointer-events-none translate-y-1/2 rounded-full bottom-0 left-1/2 bg-neutral-900 px-2 py-0.5">
-      <slot name="tooltip" />
-    </div>
-  </div>
-</template>
