9819fd11316ee0d200d58590e723101877f45e6f
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-12-03 00:22:10 +0200

    chore: update ESLint configuration and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,17 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   typescript: true,
   vue: true,
+
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 })
