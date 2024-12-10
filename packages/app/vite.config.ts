980043e70411d24115d0be694708a9b26454b79e
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-12-10 17:58:01 +0200

    feat: add nginx service to docker-compose and update configurations for backend and app

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -5,9 +5,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const APP_PORT = Bun.env.APP_PORT!
+const APP_HOST = Bun.env.APP_HOST!
+
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
+
+  server: {
+    port: +APP_PORT,
+    host: APP_HOST,
+  },
 
   plugins: [
     vue(),
--- packages/backend/main.ts
+++ packages/backend/main.ts
@@ -2,6 +2,9 @@ import { createBunHttpHandler } from 'trpc-bun-adapter'
 import { createContext } from './appContext'
 import { appRouter } from './appRouter'
 
+const BACKEND_HOST = Bun.env.BACKEND_HOST!
+const BACKEND_PORT = Bun.env.BACKEND_PORT!
+
 const bunHandler = createBunHttpHandler({
   endpoint: '/api',
 
@@ -15,6 +18,9 @@ const bunHandler = createBunHttpHandler({
 })
 
 const server = Bun.serve({
+  hostname: BACKEND_HOST,
+  port: BACKEND_PORT,
+
   fetch(request, response) {
     return bunHandler(request, response) ?? new Response('Not found', { status: 404 })
   },
