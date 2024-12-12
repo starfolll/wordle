140fb4bd3382e0c9f73cacdfc67f6b10089744d9
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-12-12 22:10:42 +0200

    feat: enhance ESLint configuration for TypeScript with consistent type definitions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  typescript: true,
   vue: true,
+
+  typescript: {
+    overrides: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+    },
+  },
 
   rules: {
     'unused-imports/no-unused-imports': 'error',
